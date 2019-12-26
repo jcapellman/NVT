34a783fba19311d282065cdcd0aa49a53ca3bc0d
Jarred Capellman <jcapellman@hotmail.com>
2019-12-26 13:58:23 -0500

    Fix: IP lookup url is now disabled if the lookup is disabled

--- src/NVT/ViewModels/MainViewModel.cs
+++ src/NVT/ViewModels/MainViewModel.cs
@@ -173,8 +173,6 @@ namespace NVT.ViewModels
             {
                 settingsManager.SettingsObject = value;
 
-                settingsManager.WriteFile();
-
                 OnPropertyChanged();
             }
         }
