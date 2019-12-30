f7f9f9f3b9fbfd7b6022e0040299ff18a5fc2719
Jarred Capellman <jcapellman@hotmail.com>
2019-12-30 15:18:17 -0600

    Added: Initial work on the Index View

---
+++ src/NVT.REST/Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NVT.REST.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
--- src/NVT.REST/Startup.cs
+++ src/NVT.REST/Startup.cs
@@ -17,13 +17,13 @@ namespace NVT.REST
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddMvc(a => a.EnableEndpointRouting = false);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseForwardedHeaders(new ForwardedHeadersOptions
@@ -36,14 +36,8 @@ namespace NVT.REST
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
