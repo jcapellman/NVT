14c6a889c4b8ff81843c203235b7ae3504779357
Jarred Capellman <jcapellman@hotmail.com>
2020-01-01 14:20:47 -0600

    Added: GitHub Latest Release API call

--- src/NVT.REST/Controllers/HomeController.cs
+++ src/NVT.REST/Controllers/HomeController.cs
@@ -1,9 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+using NVT.REST.Objects;
 
 namespace NVT.REST.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index() => View();
+        private async Task<GitHubLatestResponseItem> GetLatestRelease()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Add("User-Agent", "NVTWebApp");
+                
+                var response =
+                    await httpClient.GetAsync("https://api.github.com/repos/jcapellman/NVT/releases/latest");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return JsonSerializer.Deserialize<GitHubLatestResponseItem>(await response.Content.ReadAsStringAsync());
+                }
+
+                return null;
+            }
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var gitHubResponseItem = await GetLatestRelease();
+
+            return View(gitHubResponseItem);
+        }
     }
 }
