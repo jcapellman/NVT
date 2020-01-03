129710fcacfd511a5d51a39f75cf1b9a48e7d708
Jarred Capellman <jcapellman@hotmail.com>
2020-01-02 21:17:49 -0600

    Change: Added headers and port

--- src/NVT.REST/Program.cs
+++ src/NVT.REST/Program.cs
@@ -14,7 +14,8 @@ namespace NVT.REST
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>()
+                        .UseUrls("http://0.0.0.0:5005");
                 });
     }
 }
